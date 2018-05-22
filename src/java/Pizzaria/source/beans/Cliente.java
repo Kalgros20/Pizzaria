4213b0bbd5742a50708ba16ddb6e3052d92999d0
Marcikas <marcikas1995@gmail.com>
2018-05-22 15:35:04 -0300

    Adição do Singleton via enumeração

---
+++ src/java/Pizzaria/source/beans/Cliente.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pizzaria.source.beans;
+
+/**
+ *
+ * @author Marciano
+ */
+public enum Cliente { // Singleton implementado via enumeração
+    INSTANCE; // Constante Singleton
+    
+    private String nome;
+    private String endereco;
+    
+    Cliente(){} // Construtor private(por padrão) vazio
+       
+    public String getNome() {
+       return this.nome; 
+    }   
+
+    public String getEndereco() {
+       return this.endereco; 
+    }   
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    
+}
--- src/java/Pizzaria/source/beans/Teste.java
+++ src/java/Pizzaria/source/beans/Teste.java
@@ -11,8 +11,29 @@ package Pizzaria.source.beans;
  */
 public class Teste {
     public static void main(String[] args){
-        Pedido pizza = new Pizza("Pizza de Calabresa", 10); // atribuição dos valores da pizza
-        pizza = new Complemento("Borda Recheada", 5, pizza); // Pizza "Decorada" 
-        System.out.println("Pizza: " + pizza.getNome() + "\nValor a pagar: R$"+pizza.getPreco()); // Imprime os valor da pizza e seus complementos
+        Cliente cli = null; // criação do objeto Cliente sem instanciá-lo
+        cli.INSTANCE.setNome("Victor Marciano"); // atribuindo os valores ao objeto através dos setters
+        cli.INSTANCE.setEndereco("Rua Lourdes Rabello - Vila Galvão"); // atribuindo os valores ao objeto através dos setters
+        
+        Pedido pedido1 = new Pizza("Pizza com Decorator", 10); // atribuição dos valores da pizza no pedido
+        pedido1 = new Complemento("Borda Recheada", 5, pedido1); // Pizza "Decorada"       
+         // Chamada do objeto Singleton
+        System.out.println("Pizza: " + pedido1.getNome() + "\nValor a pagar: R$"+pedido1.getPreco()); // Imprime os valor da pizza e seus complementos
+        System.out.println("Cliente: " + cli.INSTANCE.getNome() + "\nEndereço: "+cli.INSTANCE.getEndereco());
+        System.out.println("\nEndereco do objeto na memoria: " + cli.INSTANCE.hashCode()); // exibindo o endereço do objeto na memoria
+        System.out.println("\nEndereco do pedido na memoria: " + pedido1.hashCode()); // o valor será diferente do segundo
+        // Novo Pedido
+        
+        Pedido pedido2 = new Pizza("Pizza sem Decorator", 10); // Pizza sem implementação do decorator
+        System.out.println("\nPizza: " + pedido2.getNome() + "\nValor a pagar: R$"+pedido2.getPreco()); // Imprime os valor da pizza e seus complementos
+        
+        cli.INSTANCE.setNome("Novo nome"); // sobreescreve os dados utilizando a mesma instancia
+        cli.INSTANCE.setEndereco("Novo endereco");        
+        
+        // exibindo o endereço do objeto/pedido na memoria(se o valor for o mesmo da instancia anterior é porque o Singleton está correto)
+        
+        System.out.println("Cliente: " + cli.INSTANCE.getNome() + "\nEndereço: " + cli.INSTANCE.getEndereco());       
+        System.out.println("\nEndereco do objeto na memoria: " + cli.INSTANCE.hashCode());
+        System.out.println("\nEndereco do pedido na memoria: " + pedido2.hashCode()); // o valor será diferente do primeiro
     }
 }
